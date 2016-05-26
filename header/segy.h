1d637919684a5f25ce532cc1c8aee5d6713a8b31
is <is@localhost.localdomain>
2016-05-26 17:11:20 +0800

    added segy class and sample code

--- header/segy.h
+++ header/segy.h
@@ -1,6 +1,11 @@
-struct segy{
-    char TextFileHead [3200];
-};
+#include <iostream>
+//#include <cstdint>
+#include <cstring>
+#include <fstream>
+
+
+    
+//static const struct x EmptyStruct;
 
 struct binaryFileHeader{
     int JOB_ID;                     //
@@ -17,6 +22,155 @@ struct binaryFileHeader{
     short int TRACE_SORT;
     short int VERT_SUM;
     short int SWEEP_FREQ_START;
-    short int SWEEP_FREQ_END; 
+    short int SWEEP_FREQ_END;
+    short int SWEEP_LENGTH;
+    short int SWEEP_TYPE;
+    short int SWEEP_NUM_CHANNEL;
+    short int SWEEP_TAPER_LEN_START;
+    short int SWEEP_TAPER_LEN_END;
+    short int TAPER_TYPE;
+    short int CORRELATED;
+    short int BINARY_GAIN;
+    short int AMP_RECOR;
+    short int MEASURE_SYSTEM;
+    short int IMPULSE_POLAR;
+    short int POLAR_CODE;
+    char UNNASSIGNED1 [240];
+    short int SEGY_REV_NUM;
+    short int FIXED_LEN;
+    short int NUM_EXT_HEAD;
+    char UNNASSIGNED2 [94];
 };
 
+struct traceHeader {
+    int TRACE_SEQ_GLOBAL;
+    int TRACE_SEQ_LOCAL;
+    int ORI_RECORD_NUM;
+    int TRACE_NUM_FIELD;
+    int SOURCE_POINT;
+    int ENSEMBLE_NUM;
+    int ENS_TRACE_NUM;
+    short int TRACE_CODE;
+    short int NUM_VERT_SUM;
+    short int NUM_HORZ_SUM;
+    short int DATA_USE;
+    int DIST_CENT_RECV;
+    int RECV_GRP_ELEV;
+    int SURF_ELEV_SRC;
+    int SOURCE_DEPTH;
+    int DATUM_ELEV_RECV;
+    int DATUM_ELAV_SRC;
+    int WATER_DEPTH_SRC;
+    int WATER_DEPTH_GRP;
+    short int SCALE_DEPTH;
+    short int SCALE_COOR;
+    int SRC_COOR_X;
+    int SRC_COOR_Y;
+    int GRP_COOR_X;
+    int GRP_COOR_Y;
+    short int COOR_UNIT;
+    short int WEATHER_VEL;
+    short int SWEATHER_VEL;
+    short int UPHOLE_T_SRC;
+    short int UPHOLE_T_GRP;
+    short int SRC_STA_CORRC;
+    short int GRP_STA_CORRC;
+    short int TOTAL_STA;
+    short int LAG_TIME_A;
+    short int LAG_TIME_B;
+    short int DELAY_T;
+    short int MUTE_T_STRT;
+    short int MUTE_T_END;
+    short int NUM_OF_SAMPL;
+    short int SAMPLE_INTRVL;
+    short int GAIN_TYPE;
+    short int GAIN_CONST;
+    short int GAIN_INIT;
+    short int CORRLTD;
+    short int SWEEP_FREQ_START;
+    short int SWEEP_FREQ_END;
+    short int SWEEP_LENGTH;
+    short int SWEEP_TYPE;
+    short int SWEEP_TAPER_LEN_START;
+    short int SWEEP_TAPER_LEN_END;
+    short int TAPER_TYPE;
+    short int ALIAS_FREQ;
+    short int ALIAS_SLOPE;
+    short int NOTCH_FREQ;
+    short int NOTCH_SLOPE;
+    short int LOWCUT_FREQ;
+    short int HIGHCUT_FREQ;
+    short int LOWCUT_SLOPE;
+    short int HIGHCUT_SLOPE;
+    short int YEAR;
+    short int DAY;
+    short int HOUR;
+    short int MINUTE;
+    short int SECOND;
+    short int TIME_CODE;
+    short int WEIGHT_FACT;
+    short int GEOPHNE_ROLL;
+    short int GEOPHNE_TRACE;
+    short int GEOPHNE_LAST;
+    short int GAP_SIZE;
+    short int OVER_TRAVEL;
+    int ENS_COOR_X;
+    int ENS_COOR_Y;
+    int INLINE;
+    int CROSS;
+    int SHOOTPOINT;
+    short int SHOOTPOINT_SCALE;
+    short int TRACE_UNIT;
+    char TRANSD_CONST [6];
+    short int TRANSD_UNIT;
+    short int TRACE_IDENT;
+    short int SCALE_TIME;
+    short int SRC_ORIENT;
+    char SRC_DIRECTION [6];
+    char SRC_MEASUREMT [6];
+    short int SRC_UNIT;
+    char UNNASSIGNED1 [6];
+};
+
+class segy
+{
+public:
+    segy();
+    ~segy();
+    void OpenFile(std::string);
+    void SaveFile();
+    void CloseFile();
+    void GetAllTrace();
+    void ReadAllTrace();
+    void SetTrace();
+    char TFileHead_ [3200];
+private:
+    // char TFileHead_ [3200];
+    binaryFileHeader BFileHead_;
+    char ExTFileHead_ [3200];
+    traceHeader** traceHeader_;
+    float *Trace_;
+    
+    std::string filename_;
+    std::fstream in_;
+};
+
+segy::segy()
+{
+}
+
+segy::~segy()
+{
+}
+
+void segy::OpenFile(std::string fl)
+{
+    char temp [400];
+    filename_ = fl;
+    in_.open(filename_.c_str(),std::ifstream::in);
+    
+    in_.read(TFileHead_, sizeof(TFileHead_));
+    in_.read(temp, sizeof(temp));
+    std::memcpy(&BFileHead_,temp,sizeof(temp));
+    
+}
